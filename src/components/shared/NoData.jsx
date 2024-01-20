3d8cc690c362e9ea06e4e6e347733f2972935f6b
nhasan97 <nh6069325@gmail.com>
2024-01-20 20:10:22 +0600

    deleted hight for nodata component, returned nodata component on the condition when there is no comments for particular survey, changed container component to dashboardcontainer component in survey comments component

--- src/components/shared/NoData.jsx
+++ src/components/shared/NoData.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 const NoData = ({ text }) => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center">
+    <div className="w-full flex flex-col justify-center items-center">
       <h1 className="text-6xl font-semibold">{text}</h1>
     </div>
   );
--- src/pages/Dashboard/SurveyComments.jsx
+++ src/pages/Dashboard/SurveyComments.jsx
@@ -1,10 +1,11 @@
 import { useParams } from "react-router-dom";
 import useComments from "../../hooks/useComments";
 import Loading from "../../components/shared/Loading";
-import Container from "../../components/shared/Container";
 import { Helmet } from "react-helmet-async";
 import Title from "../../components/shared/Title";
 import CommentCard from "../SurveyDetails/commentCard";
+import NoData from "../../components/shared/NoData";
+import DashboardContainer from "../../components/dashboard/shared/DashboardContainer";
 
 const SurveyComments = () => {
   //setting the title
@@ -21,22 +22,30 @@ const SurveyComments = () => {
     return <Loading></Loading>;
   }
 
-  return (
-    <Container>
-      <Helmet>
-        <title>PanaPoll | Survey Comments</title>
-      </Helmet>
+  if (fetchedComments.length > 0) {
+    return (
+      <DashboardContainer>
+        <Helmet>
+          <title>PanaPoll | Survey Comments</title>
+        </Helmet>
 
-      <div className="h-screen pt-16 pb-5">
-        <Title title={title}></Title>
-        <div className="w-full h-[80%] flex flex-col justify-start items-center gap-6 p-6 overflow-y-auto border rounded-xl">
-          {fetchedComments.map((coment) => (
-            <CommentCard key={coment._id} coment={coment}></CommentCard>
-          ))}
+        <div className="h-screen pt-16 pb-5">
+          <Title title={title}></Title>
+          <div className="w-full h-[80%] flex flex-col justify-start items-center gap-6 p-6 overflow-y-auto border rounded-xl">
+            {fetchedComments.map((coment) => (
+              <CommentCard key={coment._id} coment={coment}></CommentCard>
+            ))}
+          </div>
         </div>
-      </div>
-    </Container>
-  );
+      </DashboardContainer>
+    );
+  } else {
+    return (
+      <DashboardContainer>
+        <NoData text="No Comments"></NoData>
+      </DashboardContainer>
+    );
+  }
 };
 
 export default SurveyComments;
