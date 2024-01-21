e5f46827e7927ecbc1f5a516523987ea4758d910
nhasan97 <nh6069325@gmail.com>
2024-01-22 00:50:59 +0600

    added on scroll animation for recent surveys section

--- src/pages/Home/RecentSurveys/RecentSurveyCard.jsx
+++ src/pages/Home/RecentSurveys/RecentSurveyCard.jsx
@@ -3,7 +3,14 @@ import { Link } from "react-router-dom";
 const RecentSurveyCard = ({ survey }) => {
   const { _id, title, description, category } = survey;
   return (
-    <div className="card bg-[#95D0D4] hover:bg-[#7cbabe] shadow-xl">
+    <div
+      className="card bg-[#95D0D4] hover:bg-[#7cbabe] shadow-xl"
+      data-aos="zoom-in"
+      data-aos-delay="10"
+      data-aos-duration="1000"
+      data-aos-easing="ease-in-out"
+      data-aos-mirror="false"
+    >
       <Link to={`/survey-detail/${_id}`}>
         <div className="card-body items-start">
           <h2 className="card-title text-white text-2xl">{title}</h2>
