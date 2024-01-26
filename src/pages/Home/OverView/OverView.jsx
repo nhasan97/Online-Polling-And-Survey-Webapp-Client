b8dbcf9a65b7a10554afd73afeb0d87cae19b2d7
nhasan97 <nh6069325@gmail.com>
2024-01-26 10:18:19 +0600

    added counter animation for the  overview section

--- src/pages/Home/OverView/OverView.jsx
+++ src/pages/Home/OverView/OverView.jsx
@@ -4,6 +4,22 @@ import useAllSurveys from "../../../hooks/useAllSurveys";
 const OverView = () => {
   const [, , , filteredSurveys] = useAllSurveys();
 
+  let valueDisplays = document.querySelectorAll(".num");
+  let interval = 5000;
+
+  valueDisplays.forEach((valueDisplay) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+      startValue += 1;
+      valueDisplay.textContent = startValue;
+      if (startValue === endValue) {
+        clearInterval(counter);
+      }
+    }, duration);
+  });
+
   return (
     <Container>
       <div className="bg-[#FE7E51] w-full flex flex-col justify-center items-center mt-28 rounded-xl">
@@ -13,18 +29,31 @@ const OverView = () => {
           Overview
         </h1>
         <div className="w-full grid grid-cols-3 mb-8">
-          <div className="flex flex-col items-center justify-center gap-3 text-white">
-            <h1 className="text-8xl font-bold border-b-2 border-white">6</h1>
+          <div className="flex flex-col items-center justify-center gap-3 text-white border-r border-white">
+            <h1
+              className="text-8xl font-bold border-b-2 border-white num"
+              data-val="6"
+            >
+              6
+            </h1>
             <p className="text-2xl font-medium">Voters</p>
           </div>
 
           <div className="flex flex-col items-center justify-center gap-3 text-white">
-            <h1 className="text-8xl font-bold border-b-2 border-white">2</h1>
+            <h1
+              className="text-8xl font-bold border-b-2 border-white num"
+              data-val="2"
+            >
+              2
+            </h1>
             <p className="text-2xl font-medium">Surveyors</p>
           </div>
 
-          <div className="flex flex-col items-center justify-center gap-3 text-white">
-            <h1 className="text-8xl font-bold border-b-2 border-white">
+          <div className="flex flex-col items-center justify-center gap-3 text-white border-l border-white">
+            <h1
+              className="text-8xl font-bold border-b-2 border-white num"
+              data-val="13"
+            >
               {filteredSurveys.length}
             </h1>
             <p className="text-2xl font-medium">Surveys</p>
