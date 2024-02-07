8c42a994f4ddd990b3f2262b9fe08f691288c7f8
nhasan97 <nh6069325@gmail.com>
2024-02-07 16:06:43 +0600

    made the create survey form responsive, added two more image resources, added z-10 in Title component, added glass effect in creat survey form

--- src/pages/Dashboard/CreateSurvey.jsx
+++ src/pages/Dashboard/CreateSurvey.jsx
@@ -14,6 +14,9 @@ import { Helmet } from "react-helmet-async";
 import Loading from "../../components/shared/Loading";
 import Title from "../../components/shared/Title";
 import { MdDescription } from "react-icons/md";
+import { BiCategory } from "react-icons/bi";
+import pic1 from "../../assets/undraw_Forms_re_pkrt.png";
+import pic2 from "../../assets/undraw_Forms_re_pkrt2.png";
 
 const CreateSurvey = () => {
   const { user, loading } = useAuth();
@@ -87,83 +90,95 @@ const CreateSurvey = () => {
   }
 
   return (
-    <DashboardContainer>
-      <Helmet>
-        <title>PanaPoll | Dashboard | Create Survey</title>
-      </Helmet>
+    <div className="h-screen">
+      <DashboardContainer>
+        <Helmet>
+          <title>PanaPoll | Dashboard | Create Survey</title>
+        </Helmet>
 
-      <Title title={title}></Title>
+        <div className="w-full sm:w-[90%] h-[80%] flex flex-col justify-center items-center px-2 py-2 md:px-10 md:py-5 relative">
+          <Title title={title}></Title>
 
-      <div className="w-[90%] flex flex-col justify-center items-center px-10 py-5 border rounded-lg">
-        <form
-          className="w-full flex flex-col gap-4 text-left"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <div className="relative">
-            <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
-              <i className="fa-solid fa-t text-xl text-white"></i>
-            </div>
-            <input
-              type="text"
-              {...register("title")}
-              placeholder="Title"
-              required
-              className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
-            />
-          </div>
-
-          <div className="relative">
-            <div className="h-20 w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
-              <MdDescription className="text-2xl text-white" />
-            </div>
-            <textarea
-              type="email"
-              {...register("description")}
-              placeholder="Description"
-              required
-              className="input bg-[#a1dada41] w-full h-20 pl-16 py-3 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
-            />
-          </div>
-
-          <div className="relative">
-            <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
-              <i className="fa-solid fa-d text-xl text-white"></i>
-            </div>
-            <select
-              type="text"
-              {...register("category")}
-              placeholder="Category"
-              required
-              className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
-            >
-              {categories.map((category) => (
-                <option key={category}>{category}</option>
-              ))}
-            </select>
-          </div>
-
-          <div className="relative">
-            <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
-              <i className="fa-regular fa-calendar-days text-xl text-white"></i>
-            </div>
-            <input
-              type="date"
-              id="in4"
-              {...register("deadline")}
-              placeholder="Deadline"
-              required
-              className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
-            />
-          </div>
-
-          <input
-            type="submit"
-            value="Create"
-            className="btn w-1/2 mx-auto bg-[#FE7E51] text-lg font-medium text-white hover:text-[#FE7E51] normal-case rounded-lg"
+          <img
+            src={pic2}
+            alt=""
+            className="w-1/3 lg:hidden absolute top-0 sm:-top-20 left-0 z-0 opacity-[.5]"
           />
-        </form>
-      </div>
-    </DashboardContainer>
+          <form
+            className="bg-[#e7e7e72a] w-full flex flex-col gap-4 p-2 text-left backdrop-blur-[2px] z-10"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <div className="relative">
+              <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
+                <i className="fa-solid fa-t text-xl text-white"></i>
+              </div>
+              <input
+                type="text"
+                {...register("title")}
+                placeholder="Title"
+                required
+                className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
+              />
+            </div>
+
+            <div className="relative">
+              <div className="h-20 w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
+                <MdDescription className="text-2xl text-white" />
+              </div>
+              <textarea
+                type="email"
+                {...register("description")}
+                placeholder="Description"
+                required
+                className="input bg-[#a1dada41] w-full h-20 pl-16 py-3 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
+              />
+            </div>
+
+            <div className="relative">
+              <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
+                <BiCategory className="text-2xl text-white" />
+              </div>
+              <select
+                type="text"
+                {...register("category")}
+                placeholder="Category"
+                required
+                className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
+              >
+                {categories.map((category) => (
+                  <option key={category}>{category}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="relative">
+              <div className="h-[48px] w-[48px] flex justify-center items-center absolute top-0 left-0 bg-[#95D0D4] rounded-lg">
+                <i className="fa-regular fa-calendar-days text-xl text-white"></i>
+              </div>
+              <input
+                type="date"
+                id="in4"
+                {...register("deadline")}
+                placeholder="Deadline"
+                required
+                className="input bg-[#a1dada41] w-full pl-16 rounded-lg border focus:border-[#7DDDD9] focus:outline-none"
+              />
+            </div>
+
+            <input
+              type="submit"
+              value="Create"
+              className="btn w-1/2 mx-auto bg-[#FE7E51] text-lg font-medium text-white hover:text-[#FE7E51] normal-case rounded-lg"
+            />
+          </form>
+          <img
+            src={pic1}
+            alt=""
+            className="w-1/3 lg:hidden absolute bottom-0 sm:-bottom-20 right-0 z-0 opacity-[.5]"
+          />
+        </div>
+      </DashboardContainer>
+    </div>
   );
 };
 
